655a22b4555f7fe4298eb78e3bc21fe9f92ddc54
l-sugar <l.svugr@gmail.com>
2020-03-18 21:08:11 +0100

    introduced individual country argument to countries function

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-for(const o of["all","countries"])exports[o]=(async()=>{const t=await require("node-fetch")(`https://corona.lmao.ninja/${o}`);return await t.json()});
+const fetch = require('node-fetch');
+
+//all
+const all = async () => {
+	let result = await fetch('https://corona.lmao.ninja/all');
+	return result.json();
+};
+
+//countries
+const countries = async country => {
+	let result;
+	if (country === undefined) {
+		result = await fetch('https://corona.lmao.ninja/countries');
+	} else {
+		result = await fetch(`https://corona.lmao.ninja/${country}`);
+	}
+	return result.json();
+};
+
+module.exports = {
+	all: all,
+	countries: countries
+};
